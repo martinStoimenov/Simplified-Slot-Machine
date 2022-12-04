7790f10c6fc88cc25ccfb3c950259798a1e2664b
Martin <martin.hj15@gmail.com>
2022-12-04 13:23:36 +0200

    Add project files.

---
+++ Bede-gaming/ISymbol.cs
@@ -0,0 +1,11 @@
+﻿namespace Bede_gaming
+{
+    public interface ISymbol
+    {
+        decimal Coefficient { get; }
+        char Name { get; }
+        decimal ProbabilityPercentage { get; }
+        int ProbabilityRangeEnd { get; set; }
+        int ProbabilityRangeStart { get; set; }
+    }
+}
---
+++ Bede-gaming/Symbol.cs
@@ -0,0 +1,20 @@
+﻿namespace Bede_gaming
+{
+    public class Symbol : ISymbol
+    {
+        public char Name { get; private set; }
+        public decimal Coefficient { get; private set; }
+        public decimal ProbabilityPercentage { get; private set; }
+        public int ProbabilityRangeStart { get; set; }
+        public int ProbabilityRangeEnd { get; set; }
+
+        public Symbol(char name, decimal coefficient, decimal probabilityPercentage)
+        {
+            Name = name;
+            Coefficient = coefficient;
+            ProbabilityPercentage = probabilityPercentage;
+            ProbabilityRangeStart = 0;
+            ProbabilityRangeEnd = 0;
+        }
+    }
+}
